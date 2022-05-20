7b08d13dbac5a00142317c1a31e4483454926198
Pierre-louis Telep <pierre-louis.telep@cpe.fr>
2022-05-20 09:24:47 +0200

    fd

--- Partie 2/main.py
+++ Partie 2/main.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 import os
+
+from matplotlib.cbook import delete_masked_points
 import OpenGL.GL as GL
 import glfw
 import numpy as np
@@ -27,23 +29,35 @@ def init_context(window):
     # activation de la gestion de la profondeur
     GL.glEnable(GL.GL_DEPTH_TEST)
     # choix de la couleur de fond
-    GL.glClearColor(0.5, 0.6, 0.9, 1.0)
+    GL.glClearColor(0.5,0, 0.9, 1.0)
     print(f"OpenGL: {GL.glGetString(GL.GL_VERSION).decode('ascii')}")
 
 def init_program():
-    pass
-        
+    program = create_program_from_file('shader.vert','shader.frag')
+    GL.glUseProgram(program)
+    
 def init_data():
     pass
 
 def run(window):
     # boucle d'affichage
+    i = 0
+    j =0
+    delta = 0
+    beta = 0.2
     while not glfw.window_should_close(window):
         # nettoyage de la fenêtre : fond et profondeur
         GL.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT)
-
         #  l'affichage se fera ici
-
+        i += delta
+        j += beta
+        if i >= 1 :
+            delta = -0.01
+            beta = -0.1
+        elif i <= 0 :
+            delta = 0.01
+            beta = 0.1
+        GL.glClearColor(i*j,i, j, 1.0)
         # changement de buffer d'affichage pour éviter un effet de scintillement
         glfw.swap_buffers(window)
         # gestion des évènements
@@ -53,6 +67,46 @@ def key_callback(win, key, scancode, action, mods):
     # sortie du programme si appui sur la touche 'echap'
     if key == glfw.KEY_ESCAPE and action == glfw.PRESS:
         glfw.set_window_should_close(win, glfw.TRUE)
+
+def compile_shader(shader_content, shader_type):
+    # compilation d'un shader donn ́e selon son type
+    shader_id = GL.glCreateShader(shader_type)
+    GL.glShaderSource(shader_id, shader_content)
+    GL.glCompileShader(shader_id)
+    success = GL.glGetShaderiv(shader_id, GL.GL_COMPILE_STATUS)
+
+    if not success:
+        log = GL.glGetShaderInfoLog(shader_id).decode('ascii')
+        print(f'{25*"-"}\nError compiling shader: \n\
+            {shader_content}\n{5*"-"}\n{log}\n{25*"-"}')
+
+    return shader_id
+
+def create_program( vertex_source, fragment_source):
+    # creation d'un programme gpu
+    vs_id = compile_shader(vertex_source, GL.GL_VERTEX_SHADER)
+    fs_id = compile_shader(fragment_source, GL.GL_FRAGMENT_SHADER)
+    if vs_id and fs_id:
+        program_id = GL.glCreateProgram()
+        GL.glAttachShader(program_id, vs_id)
+        GL.glAttachShader(program_id, fs_id)
+        GL.glLinkProgram(program_id)
+        success = GL.glGetProgramiv(program_id, GL.GL_LINK_STATUS)
+        if not success:
+            log = GL.glGetProgramInfoLog(program_id).decode('ascii')
+            print(f'{25*"-"}\nError linking program:\n{log}\n{25*"-"}')
+            GL.glDeleteShader(vs_id)
+            GL.glDeleteShader(fs_id)
+    return program_id
+
+def create_program_from_file(vs_file, fs_file):
+    # creation d'un programme gpu `a partir de fichiers
+    vs_content = open(vs_file, 'r').read() if os.path.exists(vs_file)\
+        else print(f'{25*"-"}\nError reading file:\n{vs_file}\n{25*"-"}')
+    fs_content = open(fs_file, 'r').read() if os.path.exists(fs_file)\
+        else print(f'{25*"-"}\nError reading file:\n{fs_file}\n{25*"-"}')
+
+    return create_program(vs_content, fs_content)
 
 def main():
     window = init_window()
